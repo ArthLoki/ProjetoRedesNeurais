5950e2a577c90272fc18b682df4c2bf8a9657037
Luiza Bretas <80011623+ArthLoki@users.noreply.github.com>
2023-12-26 23:56:45 -0300

    bugFix csv

--- T1/getFormatedExifData.py
+++ T1/getFormatedExifData.py
@@ -1,14 +1,96 @@
 from ProjetoRedesNeurais.auxiliary_func.getPath import get_base_path
+# from ProjetoRedesNeurais.auxiliary_func.getIndexes import getDateIndexes, findIndexFilePath
 import os
 
-def readTXT(path, filename):
+# Global variables
+# 1 - Path
+global base_path, data_path, csv_path, csv_total_path
+base_path = get_base_path()
+data_path = f'{base_path}/Images/T1'
+csv_path = f'{base_path}/T1'
+
+# 2 - Files
+global filenames, csv_filename
+filenames = [name for name in os.listdir(data_path) if os.path.splitext(name)[-1] == '.jpg']
+csv_filename = 'output.csv'
+csv_total_path = f'{csv_path}/{csv_filename}'
+
+
+def getSpecificIndexes(input):
+    file = openTXT(data_path, filenames[0])
+    header = createContentList(file, 0)
+
+    indexes = []
+    for i, label in enumerate(header):
+        if input in label:
+            indexes.append(i)
+    return indexes
+
+
+def getMultSpecificIndexes(lInput):
+    indexes = []
+    for input in lInput:
+        indexes.append(i for i in getSpecificIndexes(input) if i not in indexes)
+    return indexes
+
+
+def getDateIndexes():
+    dateIndexes = getSpecificIndexes('Date')
+    return dateIndexes
+
+
+def findIndexFilePath():
+    FilePathIndex = getSpecificIndexes('FilePath')
+    return FilePathIndex[0]
+
+
+def openTXT(path, filename):
     file_path = f'{path}/{filename}.txt'
     file = open(file_path, 'rb')
 
     return file
 
 
-def fixDataContentList(lData):
+def processingData(content, escolha_tratamento):
+    filePathIndex = findIndexFilePath()
+
+    if escolha_tratamento == 0:
+        # Replaces \\ to /
+        content[filePathIndex] = content[filePathIndex].replace('\\\\', '/')
+    if escolha_tratamento == 1:
+        # Removes the FilePath data from dataset
+        content.pop(filePathIndex)
+    elif escolha_tratamento == 2:
+        # Makes the header label shorter
+        for i, h in enumerate(content):
+            content[i] = h.split('/')[-1]
+    return content
+
+
+def writeCSV(content, modo, is_header=False):
+    file = open(csv_total_path, modo)
+
+    content = processingData(content, 1)
+
+    # if is_header:
+    #     content = processingData(content, 2)
+
+    if type(content) == list:
+        file.write(content[0])
+        for c in content[1:]:
+            file.write(f';{c}')
+        file.write('\n')
+    file.close()
+    return
+
+def getHeaderList():
+    file = openTXT(data_path, filenames[0])
+    header = createContentList(file, 0)
+    # file.close()
+    return header
+
+
+def cleanDataContentList(lData):
     lDataFixed = []
     for i in range(len(lData)):
         lDataFixed.append(lData[i].split('\"')[1])
@@ -19,63 +101,39 @@ def createContentList(file, content_type):
     content = []
     for line in file:
         lData = str(line).strip().split(',')
-        lData = fixDataContentList(lData)
-        # print(f'lData: {lData}')
+        lData = cleanDataContentList(lData)
 
         if content_type == 0:  # wants to get header list
-            # print(f'header: {lData[0]}')
             content.append(lData[0])
         elif content_type == 1:
-            # print(f'data: {lData[1]}')
             content.append(lData[1])  # wants to get data list
         else:
             print("Escolha um tipo valido")
     return content
 
 
-def writeCSV(path, filename, content, modo, is_exif_data=True):
-    file = open(f'{path}/{filename}.csv', modo)
-
-    if is_exif_data and modo == 'a':
-        content[0] = content[0].replace('\\\\', '/')
-
-    if type(content) == list:
-        file.write(content[0])
-        for c in content[1:]:
-            file.write(f';{c}')
-        file.write('\n')
-    file.close()
-    return
+def getContentList(filename):
+    file = openTXT(data_path, filename)
+    content = createContentList(file, 1)
+    # file.close()
+    return content
 
 
-def getPathIndexes(header):
-    pathIndexes = []
-    for i in header:
-        if header[i].endsith('Path'):
-            pathIndexes.append(i)
-    return pathIndexes
-
-
-def main():
-    base_path = get_base_path()
-    data_path = f'{base_path}/Images/T1'
-    csv_path = f'{base_path}/T1'
-
-    filenames = [name for name in os.listdir(data_path) if os.path.splitext(name)[-1] == '.jpg']
+def generateExifDataset():
+    findIndexFilePath()
+    getDateIndexes()
 
     # Run this part only once to add header
-    file = readTXT(data_path, filenames[0])
-    header = createContentList(file, 0)
-    writeCSV(csv_path, 'output', header, 'w', False)
+    header = getHeaderList()
+    # print("Header: ", header)
+    writeCSV(header, 'w')
 
     for filename in filenames:
-        file = readTXT(data_path, filename)
-        content = createContentList(file, 1)
-        writeCSV(csv_path, 'output', content, 'a', True)
-
-    file.close()
+        content = getContentList(filename)
+        # print(f"Content ({filename}): ", content)
+        writeCSV(content, 'a')
 
 
 if __name__ == '__main__':
-    main()
+    generateExifDataset()
 
--- T1/identifyImages.py
+++ T1/identifyImages.py
@@ -5,56 +5,45 @@ import torch
 import csv
 import os
 
-global base_path, data_path
-base_path = get_base_path()
-data_path = f'{base_path}/ProjetoRedesNeurais'
+from getFormatedExifData import generateExifDataset, getSpecificIndexes, getMultSpecificIndexes
+from getFormatedExifData import base_path, data_path, csv_path, csv_filename, filenames
 
 
-def importCSVData(csv_path):
+def importCSVData():
     csv_data_numpy = np.loadtxt(
-        csv_path,
+        csv_path+'/'+csv_filename,
         dtype=np.float32,
         delimiter=';',
         skiprows=1
     )
     return torch.from_numpy(csv_data_numpy)  # Returns a PyTorch Tensor
 
-
-def getImageIdentifierColumnList():
-    global data_path
-
-    data_dir = f'{data_path}/Images/T1'
-    imageIdentifierColumnList = [name for name in os.listdir(data_dir) if os.path.splitext(name)[-1] == '.jpg']
-
-    return imageIdentifierColumnList
-
-
-def getOgColumnList():
-    ogColumnList = []
-
-    # 1 - get image filenames, creating a list
-    imageIdentifierColumnList = getImageIdentifierColumnList()
-
-    # 2 - check if 'og' is in each filename in this list. If so, append 1.0 to the ogColumnList, else append 0.0
-    for name in imageIdentifierColumnList:
-        if 'og' in name:
-            ogColumnList.append(1.0)
-        else:
-            ogColumnList.append(0.0)
-
-    # 3 - return ogColumnList
-    return ogColumnList
+# REFORMULAR
+# def getOgColumnList():
+#     ogColumnList = []
+#
+#     # 1 - get image filenames, creating a list
+#     imageIdentifierColumnList = getImageIdentifierColumnList()
+#
+#     # 2 - check if 'og' is in each filename in this list. If so, append 1.0 to the ogColumnList, else append 0.0
+#     for name in imageIdentifierColumnList:
+#         if 'og' in name:
+#             ogColumnList.append(1.0)
+#         else:
+#             ogColumnList.append(0.0)
+#
+#     # 3 - return ogColumnList
+#     return ogColumnList
 
 
 def main():
-    global data_path
     filename = 'output.csv'
-    csv_img_path = f'{base_path}/T1/{filename}'
+
+    generateExifDataset()
 
     # import csv data to a pytorch tensor
-    # img_data = importCSVData(f'C:/Users/lubjc/PycharmProjects/DeepLearning/ProjetoRedesNeurais/T1/{filename}')
-    img_data = importCSVData(csv_img_path)
-    print(img_data.shape)
+    # exif_data = importCSVData()
+    # print(exif_data.shape)
 
 
 if __name__ == '__main__':
